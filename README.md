e71bc2a8ce42eb1b5cd760686173be307de95d70
Andrew Healey <healeycodes@gmail.com>
2022-01-02 12:39:51 +0000

    Add branch support

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ For now, here's ~100 lines of Go that host your source files.
 This is a good example use-case of `sync.WaitGroup` – all clones are run as concurrent goroutines.
 
 ```bash
-PORT=":80" GITHUB_USERNAME="healeycodes" go run serve.go
+PORT="80" GITHUB_USERNAME="healeycodes" go run serve.go
 # optionally use PER_PAGE to raise the number of repositories (up to 100)
 # TODO: pagination for unlimited repositories
 ```
--- serve.go
+++ serve.go
@@ -2,57 +2,70 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path"
-	"time"
+	"strings"
+	"sync"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/gorilla/mux"
 )
-
-// TODOs
 
 // - paginate GH API to get _all_ repos
 
-// (not sure best order for the following:)
-// - serve tree
-// - serve branches
-// - serve code files
-
-// - syntax highlighting (maybe?)
-
-const updateFrequency = 15 // seconds
+const updateFrequency = 300 // seconds
 const repoPath = "./repositories/"
 
+var repoMutexes map[string]*sync.Mutex
+
 func main() {
+	repoMutexes = make(map[string]*sync.Mutex)
 	if err := os.Mkdir(repoPath, 0755); err != nil && !os.IsExist(err) {
 		log.Fatal(err)
 	}
 
-	err := updateRepos()
-	if err != nil {
-		log.Printf("error: %s\n", err)
-	}
+	// err := updateRepos()
+	// if err != nil {
+	// 	log.Printf("error: %s\n", err)
+	// }
 
-	ticker := time.NewTicker(updateFrequency * time.Second)
-	go func() {
-		for {
-			select {
-			case <-ticker.C:
-				err = updateRepos()
-				if err != nil {
-					log.Printf("error: %s\n", err)
-				}
-			}
-		}
-	}()
+	// ticker := time.NewTicker(updateFrequency * time.Second)
+	// go func() {
+	// 	for {
+	// 		<-ticker.C
+	// 		err = updateRepos()
+	// 		if err != nil {
+	// 			log.Printf("error: %s\n", err)
+	// 		}
+	// 	}
+	// }()
 
 	serve()
 }
 
-func getGitHubInfo() (repos, error) {
+func lockRepo(repo string) {
+	if m, found := repoMutexes[repo]; found {
+		m.Lock()
+	} else {
+		m := &sync.Mutex{}
+		m.Lock()
+		repoMutexes[repo] = m
+	}
+}
+
+func unlockRepo(repo string) {
+	if m, found := repoMutexes[repo]; found {
+		m.Unlock()
+	}
+}
+
+func getGitHubInfo() (gitHubRepos, error) {
 	username, found := os.LookupEnv("GITHUB_USERNAME")
 	if !found {
 		log.Fatal("GITHUB_USERNAME not set")
@@ -75,25 +88,158 @@ func getGitHubInfo() (repos, error) {
 		return nil, err
 	}
 
-	var repos repos
+	var repos gitHubRepos
 	err = json.Unmarshal(b, &repos)
 	if err != nil {
+		log.Printf("%s\n", resp.Status)
+		log.Printf("%s\n", url)
+		log.Printf("%s\n", string(b))
 		return nil, err
 	}
 
 	return repos, nil
 }
 
-func cloneRepo(name string, gitUrl string) error {
-	_, err := git.PlainClone(repoPath+name, false, &git.CloneOptions{
+func cloneRepo(repoName string, gitUrl string) error {
+	_, err := git.PlainClone(repoPath+repoName, false, &git.CloneOptions{
 		URL:      gitUrl,
 		Progress: log.Writer(),
 	})
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
+
+func allLocalRepos() (map[string]bool, error) {
+	files, err := os.ReadDir(repoPath)
+	if err != nil {
+		return nil, err
+	}
+	directories := make(map[string]bool)
+	for _, file := range files {
+		if file.IsDir() {
+			directories[file.Name()] = true
+		}
+	}
+	return directories, nil
+}
+
+// Get branches, files, and commits, for a repository and optional extra path
+func getContext(repoName string, extra string) ([]string, []gitFile, []gitCommit, error) {
+	localGitPath := path.Join(repoPath, repoName)
+	if extra != "" {
+		localGitPath = path.Join(localGitPath, extra)
+	}
+
+	// Files
+	fileList := make([]gitFile, 0)
+	files, err := ioutil.ReadDir(localGitPath)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	for _, file := range files {
+		if file.IsDir() && file.Name() == ".git" {
+			continue
+		}
+		fileList = append(fileList, gitFile{
+			Name:  file.Name(),
+			IsDir: file.IsDir(),
+		})
+	}
+
+	// Branches
+	branchList := make([]string, 0)
+	r, err := git.PlainOpen(localGitPath)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	refIter, _ := r.References()
+	refIter.ForEach(func(ref *plumbing.Reference) error {
+		if ref.Name().IsRemote() {
+			branchShort := strings.Replace(ref.Name().Short(), "origin/", "", 1)
+			branchList = append(branchList, branchShort)
+		}
+		return nil
+	})
+
+	// Commits
+	commitList := make([]gitCommit, 0)
+	commitIter, err := r.Log(&git.LogOptions{})
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	commitIter.ForEach(func(c *object.Commit) error {
+		commitList = append(commitList, gitCommit{
+			Hash:    c.Hash,
+			Author:  c.Author.Name,
+			Message: c.Message,
+		})
+		return nil
+	})
+
+	return branchList, fileList, commitList, nil
+}
+
+func renderContext(repository string, branch string, branchList []string, fileList []gitFile, commitList []gitCommit) string {
+	ret := "<html>"
+
+	// Branches
+	ret += "<h3>branches</h3><ul>"
+	for _, branchShort := range branchList {
+		if branchShort == branch {
+			ret += fmt.Sprintf("<li>%s</li>", branch)
+		} else {
+			ret += fmt.Sprintf("<li><a href=\"/%s/?branch=%s\">%s</a></li>", repository, branchShort, branchShort)
+		}
+	}
+	ret += "</ul>"
+
+	// Files
+	ret += "<h3>files</h3><ul>"
+	for _, gf := range fileList {
+		var link string
+		var text string
+		if gf.IsDir {
+			link = fmt.Sprintf("%s/%s/?branch=%s", repository, gf.Name, branch)
+			text = fmt.Sprintf("<li><a href=\"/%s\">%s/</a></li>", link, gf.Name)
+		} else {
+			link = fmt.Sprintf("%s/%s?branch=%s", repository, gf.Name, branch)
+			text = fmt.Sprintf("<li><a href=\"/%s\">%s</a></li>", link, gf.Name)
+		}
+		ret += text
+	}
+	ret += "</ul>"
+
+	// Commits
+	ret += "<h3>log</h3><ul>"
+	for _, c := range commitList {
+		ret += fmt.Sprintf("<li><a href=\"\">%s</a></li>", c.Message)
+	}
+	ret += "</ul>"
+
+	return ret + "</html>"
+}
+
+// func renderCommit(repoName string, hash plumbing.Hash) (string, error) {
+// 	localGitPath := path.Join(repoPath, repoName)
+// 	r, err := git.PlainOpen(localGitPath)
+// 	if err != nil {
+// 		return "", err
+// 	}
+
+// 	c, err := r.CommitObject(hash)
+// 	if err != nil {
+// 		return "", err
+// 	}
+
+// 	println(c.String())
+// 	return c.String(), nil
+// }
 
 func updateRepos() error {
 	githubRepos, err := getGitHubInfo()
@@ -101,15 +247,9 @@ func updateRepos() error {
 		return err
 	}
 
-	files, err := os.ReadDir(repoPath)
+	directories, err := allLocalRepos()
 	if err != nil {
 		return err
-	}
-	directories := make(map[string]bool)
-	for _, file := range files {
-		if file.IsDir() {
-			directories[file.Name()] = true
-		}
 	}
 
 	for _, repo := range githubRepos {
@@ -153,20 +293,169 @@ func updateRepos() error {
 	return nil
 }
 
+// Get branch from short name (or get default branch if branchShort is "")
+func getBranchRefFromShort(repoName string, branchShort string) (plumbing.Hash, string, error) {
+	localGitPath := path.Join(repoPath, repoName)
+	r, err := git.PlainOpen(localGitPath)
+	if err != nil {
+		return plumbing.Hash{}, "", err
+	}
+
+	var refHash plumbing.Hash
+
+	if branchShort == "" {
+		branches, err := r.Branches()
+		if err != nil {
+			return plumbing.Hash{}, "", err
+		}
+
+		ref, err := branches.Next()
+		if err != nil {
+			return plumbing.Hash{}, "", err
+		}
+
+		head, err := r.Head()
+		if err != nil {
+			return plumbing.Hash{}, "", err
+		}
+
+		return head.Hash(), ref.Name().Short(), nil
+	}
+
+	refIter, _ := r.References()
+	refIter.ForEach(func(ref *plumbing.Reference) error {
+		if ref.Name().IsRemote() {
+			if branchShort == strings.Replace(ref.Name().Short(), "origin/", "", 1) {
+				refHash = ref.Hash()
+			}
+		}
+		return nil
+	})
+
+	if refHash.IsZero() {
+		return plumbing.Hash{}, "", fmt.Errorf("couldn't find branch: %s", branchShort)
+	}
+
+	return refHash, branchShort, nil
+}
+
+// Checkout a branch, return the branch short name
+func checkoutBranch(repoName string, branchShort string) (string, error) {
+	localGitPath := path.Join(repoPath, repoName)
+	r, err := git.PlainOpen(localGitPath)
+	if err != nil {
+		return "", err
+	}
+
+	branchRef, branchShort, err := getBranchRefFromShort(repoName, branchShort)
+	if err != nil {
+		return "", err
+	}
+
+	w, err := r.Worktree()
+	if err != nil {
+		return "", err
+	}
+
+	err = w.Checkout(&git.CheckoutOptions{Hash: branchRef})
+	if err != nil {
+		return "", err
+	}
+
+	return branchShort, nil
+}
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	directories, err := allLocalRepos()
+	if err != nil {
+		log.Printf("error: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	list := make([]string, 0)
+	for dir := range directories {
+		list = append(list, dir)
+	}
+	json.NewEncoder(w).Encode(list)
+}
+
+func handleRepository(w http.ResponseWriter, r *http.Request) {
+	repository := mux.Vars(r)["repository"]
+	filePath := mux.Vars(r)["path"]
+	branch := r.URL.Query().Get("branch")
+
+	lockRepo(repository)
+	defer unlockRepo(repository)
+
+	currentBranchShort, err := checkoutBranch(repository, branch)
+	if err != nil {
+		log.Printf("error: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if strings.HasSuffix(filePath, "/") || filePath == "" {
+		branchList, fileList, commitList, err := getContext(repository, filePath)
+		if err != nil {
+			log.Printf("error: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.Write([]byte(renderContext(path.Join(repository, filePath), currentBranchShort, branchList, fileList, commitList)))
+	} else {
+		http.ServeFile(w, r, path.Join(repoPath, repository, filePath))
+	}
+}
+
+// func handleCommit(w http.ResponseWriter, r *http.Request) {
+// 	repository := mux.Vars(r)["repository"]
+// 	hash := mux.Vars(r)["hash"]
+
+// 	lockRepo(repository)
+// 	defer unlockRepo(repository)
+
+// 	// TODO: checkout commit
+
+// 	// TODO: render commit
+// }
+
 func serve() {
-	http.Handle("/", http.FileServer(http.Dir(repoPath)))
 	port, found := os.LookupEnv("PORT")
 	if !found {
 		log.Fatal("PORT not set")
 	}
 
-	log.Printf("Serving on http://localhost%s\n", port)
-	if err := http.ListenAndServe(port, nil); err != nil {
-		log.Fatal(err)
+	router := mux.NewRouter()
+	router.HandleFunc("/", handleHome)
+	router.HandleFunc("/{repository}/{path:.?|.+}", handleRepository)
+
+	// TODO: rework links to use /tree/
+	// router.HandleFunc("/{repository}/tree/{path:.?|.+}", handleRepoPath)
+
+	// TODO: add commit handling
+	// router.HandleFunc("/{repository}/commit/{path:.?|.+}", handleCommit)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    fmt.Sprintf("0.0.0.0:%s", port),
 	}
+	log.Fatal(srv.ListenAndServe())
 }
 
-type repos []struct {
+type gitCommit struct {
+	Hash    plumbing.Hash
+	Author  string
+	Message string
+}
+
+type gitFile struct {
+	Name  string
+	IsDir bool
+}
+
+type gitHubRepos []struct {
 	GitUrl string `json:"git_url"`
 	Name   string `json:"name"`
 }
