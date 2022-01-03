6774951af68ad88c6bda09179d378fe5d051bb36
Andrew Healey <healeycodes@gmail.com>
2022-01-03 20:48:25 +0000

    Run seed script in CI

---
+++ test/seed.sh
@@ -0,0 +1,15 @@
+# setup git repository at test/repositories/a_repository
+
+mkdir repositories
+cd repositories
+mkdir a_repository
+cd a_repository
+git init
+git checkout -b one
+echo "a" > a.txt 
+git add .
+git commit -m "Add a"
+git checkout -b two
+echo "b" > b.txt 
+git add .
+git commit -m "Add v"
